7f66b76d836a85c66f85361f254f43e42be681fc
Daniel <daniel@Daniels-iMac-Pro.local>
2024-11-04 11:48:50 -0300

    feat: add user creation route

---
+++ src/routes/users.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { knex } from '../database/connection'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUsersBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    const { name, email } = createUsersBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 14, // 14 Days
+      })
+    }
+
+    const findUserByEmail = await knex('users').where({ email }).first()
+
+    if (findUserByEmail) {
+      return reply
+        .status(400)
+        .send({ message: 'The provided user is already registered.' })
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
