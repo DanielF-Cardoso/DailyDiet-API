1a71e6a4fb57fe9ddb27cf40c711fce9fe208a41
DanielF-Cardoso <danielfca122@gmail.com>
2024-11-06 21:04:38 -0300

    feat: add meal edit route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -30,5 +30,41 @@ export async function mealsRoutes(app: FastifyInstance) {
       })
       return reply.status(201).send
     },
+  ),
+
+  app.put(
+    '/:id', 
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const updateMealsBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        inDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, inDiet, date } = updateMealsBodySchema.parse(
+        request.body,
+      )
+
+      const meal = await knex('meals').where({ id: id }).first()
+
+      if (!meal) {
+        return reply.status(404).send({ message: 'The Meal was not found' })
+      }
+
+      await knex('meals').update({
+        name,
+        description,
+        in_diet: inDiet,
+        date: date.getTime(),
+      }).where({ id, user_id: request.user?.id })
+
+      return reply.status(204).send()
+    },
   )
 }
