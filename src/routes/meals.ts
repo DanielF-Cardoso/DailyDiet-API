2c3193ec96c04106074702881b31ce590fb8b7a8
DanielF-Cardoso <danielfca122@gmail.com>
2024-11-06 21:43:25 -0300

    feat: add routes for managing meals (get, delete, details, metrics)

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -66,5 +66,81 @@ export async function mealsRoutes(app: FastifyInstance) {
 
       return reply.status(204).send()
     },
+  ),
+
+  app.delete(
+    '/:id', 
+    { preHandler: [checkSessionIdExists] }, 
+    async (request, reply) => {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals').where({ id: id }).first()
+
+      if (!meal) {
+        return reply.status(404).send({ message: 'The Meal was not found' })
+      }
+
+      await knex('meals').delete().where({ id, user_id: request.user?.id })
+
+      return reply.status(204).send()
+    },
+  ),
+
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] }, 
+    async (request, reply) => {
+      const meals = await knex('meals').where({ user_id: request.user?.id })
+
+      return reply.send({ meals })
+    },      
+  ),
+
+  app.get(
+    '/:id',
+    { preHandler: [checkSessionIdExists] }, 
+    async (request, reply) => {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals').where({ id, user_id: request.user?.id }).first()
+
+      if (!meal) {
+          return reply.status(404).send({ message: 'The Meal was not found' })
+      }
+
+      return reply.send({ meal })
+    },
+  ),
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const meals = await knex('meals')
+      .where({ user_id: request.user?.id })
+      .orderBy('date', 'desc')
+
+      const totalMeals = meals.length
+      const totalMealsOnDiet = meals.filter(meal => meal.in_diet).length
+      const totalMealsOffDiet = totalMeals - totalMealsOnDiet
+
+      let bestOnDietSequence = 0
+      let currentSequence = 0
+  
+      meals.forEach(meal => {
+        if (meal.in_diet) {
+          currentSequence += 1
+          bestOnDietSequence = Math.max(bestOnDietSequence, currentSequence)
+        } else {
+          currentSequence = 0
+        }
+      })
+
+      return reply.send({ totalMeals, totalMealsOnDiet, totalMealsOffDiet, bestOnDietSequence })
+    },
   )
 }
